b518b8c75f760c43cf4bf1171bb8fcf57d2ae22e
6M21JH2\jbarra <relicorb@gmail.com>
2018-06-07 16:46:29 -0400

    Facebook Login Works

--- app/src/main/java/com/ciphra/android/memesmash/MainActivity.java
+++ app/src/main/java/com/ciphra/android/memesmash/MainActivity.java
@@ -13,6 +13,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -22,9 +29,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,15 +49,18 @@ public class MainActivity extends AppCompatActivity {
     private Button cancelLoginEmail;
     private Button registerWithEmail;
     private Button registerWithGoogle;
-    private Button registerWithFacebook;
+    private LoginButton registerWithFacebook;
     private Button cancelRegistration;
     private FirebaseUser user;
 
+    private CallbackManager callbackManager;
     private EditText emailEditText;
     private EditText emailPasswordEditText;
     private EditText emailLoginEditText;
     private EditText emailPasswordLoginEditText;
     private EditText emailPasswordConfirmBox;
+    private static final String EMAIL = "email";
+
     public static final int RC_SIGN_IN = 0;
 
     @Override
@@ -55,6 +68,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
     }
 
     @Override
@@ -265,6 +297,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+       registerWithFacebook = findViewById(R.id.login_button);
+       registerWithFacebook.setReadPermissions(Arrays.asList(EMAIL));
+       registerWithFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+           @Override
+           public void onSuccess(LoginResult loginResult) {
+               handleFacebookAccessToken(loginResult.getAccessToken());
+
+           }
+
+           @Override
+           public void onCancel() {
+
+           }
+
+           @Override
+           public void onError(FacebookException error) {
+
+           }
+       });
+
 
     }
 
@@ -287,6 +339,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
@@ -334,6 +387,28 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         // ...
+                    }
+                });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            updateUI(null);
+                        }
                     }
                 });
     }
