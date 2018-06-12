03edd7a752e1a952c5be7acd208a81dfed5de063
6M21JH2\jbarra <relicorb@gmail.com>
2018-06-12 11:17:32 -0400

    Made Login/Register

--- app/src/main/java/com/ciphra/android/memesmash/AnonymousMemeActivity.java
+++ app/src/main/java/com/ciphra/android/memesmash/AnonymousMemeActivity.java
@@ -1,13 +1,33 @@
 package com.ciphra.android.memesmash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AnonymousMemeActivity extends AppCompatActivity {
+
+    private TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anonymous_meme);
+
+        mLoginTextView = findViewById(R.id.anonymous_greeting_textview);
+        mLoginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goLogin();
+            }
+        });
     }
+
+    public void goLogin(){
+        Intent intent = new Intent(this, LoginRegister.class);
+        startActivity(intent);
+    }
+
+
 }
